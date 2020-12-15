07f96900de65ec9c6e5ac25261d4dea76a6d87b0
Adam Alexandru <adamalexandru4@yahoo.com>
2020-12-15 17:44:40 +0200

    Add document UI/UX updates

--- src/app/documents/components/document-form/document-form.component.ts
+++ src/app/documents/components/document-form/document-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { DocumentMetadata, DocumentType } from '@app/core/models';
 
 @Component({
@@ -18,7 +18,7 @@ export class DocumentFormComponent implements OnInit, OnChanges {
   public url = '/file';
   public documentForm = new FormGroup({
     documentTitle: new FormControl('', [Validators.required]),
-    documentNumber: new FormControl('', [Validators.required]),
+    documentNumber: new FormControl('', [Validators.required, Validators.min(1)]),
     documentInitializer: new FormControl('', [Validators.required]),
     documentType: new FormControl('', [Validators.required]),
     dateOfDevelopment: new FormControl('', [Validators.required]),
@@ -39,14 +39,32 @@ export class DocumentFormComponent implements OnInit, OnChanges {
   }
 
   public onSave() {
-    const documentMetadata = new DocumentMetadata();
-    documentMetadata.id = this.document.id;
-    documentMetadata.fromFormData(this.documentForm.value);
-    this.save.emit(documentMetadata);
+    if (this.documentForm.valid) {
+      const documentMetadata = new DocumentMetadata();
+      documentMetadata.id = this.document.id;
+      documentMetadata.fromFormData(this.documentForm.value);
+      this.save.emit(documentMetadata);
+    }
   }
 
   public documentTypeKeys(): string[] {
     const keys = Object.keys(DocumentType);
     return keys.slice(keys.length / 2);
+  }
+
+  public hasError(control: AbstractControl) {
+    return (control.invalid && (control.dirty || control.touched));
+  }
+
+  public isValidClassRequired(control: AbstractControl) {
+    return this.hasError(control) && control.errors.required ? 'input-group-invalid' : '';
+  }
+
+  public isValidClassNumber(control: AbstractControl) {
+    return this.hasError(control) && control.errors.min ? 'input-group-invalid' : '';
+  }
+
+  public isFilePathValid(control: AbstractControl) {
+    console.log(control);
   }
 }
